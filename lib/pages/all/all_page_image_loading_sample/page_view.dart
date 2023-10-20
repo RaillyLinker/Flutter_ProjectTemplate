23c352aa685c7eecb1ff3b0330e0a287af779588
RaillyLinker <raillylinker@gmail.com>
2023-10-20 11:24:54 +0900

    -

--- lib/pages/all/all_page_image_loading_sample/page_view.dart
+++ lib/pages/all/all_page_image_loading_sample/page_view.dart
@@ -49,148 +49,190 @@ class PageView extends StatelessWidget {
         ),
       ),
       backgroundColor: const Color(0xFFFFFFFF),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.only(top: 80),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Column(
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    decoration: const BoxDecoration(
-                      border: Border(
-                        top: BorderSide(width: 1.0, color: Colors.black),
-                        bottom: BorderSide(width: 1.0, color: Colors.black),
-                        left: BorderSide(width: 1.0, color: Colors.black),
-                        right: BorderSide(width: 1.0, color: Colors.black),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Center(
+          child: Container(
+            padding: const EdgeInsets.only(top: 80, left: 20, right: 20),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          top: BorderSide(width: 1.0, color: Colors.black),
+                          bottom: BorderSide(width: 1.0, color: Colors.black),
+                          left: BorderSide(width: 1.0, color: Colors.black),
+                          right: BorderSide(width: 1.0, color: Colors.black),
+                        ),
+                      ),
+                      child: Image(
+                        // 바로 이미지를 내려주는 샘플
+                        image: const NetworkImage(
+                            "http://127.0.0.1:8080/service1/tk/v1/file-test/client-image-test?delayTimeSecond=0"),
+                        fit: BoxFit.cover,
+                        loadingBuilder: (BuildContext context, Widget child,
+                            ImageChunkEvent? loadingProgress) {
+                          // 로딩 중일 때 플레이스 홀더를 보여줍니다.
+                          if (loadingProgress == null) {
+                            return child; // 로딩이 끝났을 경우
+                          }
+                          return Container(
+                            color: Colors.grey,
+                            child: const Center(
+                              child: CircularProgressIndicator(),
+                            ),
+                          );
+                        },
+                        errorBuilder: (context, error, stackTrace) {
+                          // 에러 발생 시 설정한 에러 위젯을 반환합니다.
+                          return Container(
+                            color: Colors.grey,
+                            child: const Center(
+                              child: Icon(
+                                Icons.error,
+                                color: Colors.red,
+                              ),
+                            ),
+                          );
+                        },
                       ),
                     ),
-                    child: FadeInImage(
-                      fit: BoxFit.cover,
-                      placeholder: const AssetImage(
-                          "lib/assets/images/loading_spinner.gif"),
-                      image: const NetworkImage(
-                          "http://127.0.0.1:8080/service1/tk/v1/file-test/client-image-test?delayTimeSecond=0"),
-                      imageErrorBuilder: (context, error, stackTrace) {
-                        return Container(
-                          color: Colors.grey,
-                          child: const Center(
-                            child: Icon(
-                              Icons.error,
-                              color: Colors.red,
-                            ),
-                          ),
-                        );
-                      },
+                    const SizedBox(
+                      height: 5,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  const Center(
-                    child: Text(
-                      "바로 로딩",
-                      style: TextStyle(fontFamily: "MaruBuri"),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                width: 50,
-              ),
-              Column(
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    decoration: const BoxDecoration(
-                      border: Border(
-                        top: BorderSide(width: 1.0, color: Colors.black),
-                        bottom: BorderSide(width: 1.0, color: Colors.black),
-                        left: BorderSide(width: 1.0, color: Colors.black),
-                        right: BorderSide(width: 1.0, color: Colors.black),
+                    const Center(
+                      child: Text(
+                        "바로 로딩",
+                        style: TextStyle(fontFamily: "MaruBuri"),
                       ),
                     ),
-                    child: FadeInImage(
-                      fit: BoxFit.cover,
-                      placeholder: const AssetImage(
-                          "lib/assets/images/loading_spinner.gif"),
-                      image: const NetworkImage(
-                          "http://127.0.0.1:8080/service1/tk/v1/file-test/client-image-test?delayTimeSecond=5"),
-                      imageErrorBuilder: (context, error, stackTrace) {
-                        return Container(
-                          color: Colors.grey,
-                          child: const Center(
-                            child: Icon(
-                              Icons.error,
-                              color: Colors.red,
+                  ],
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Column(
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          top: BorderSide(width: 1.0, color: Colors.black),
+                          bottom: BorderSide(width: 1.0, color: Colors.black),
+                          left: BorderSide(width: 1.0, color: Colors.black),
+                          right: BorderSide(width: 1.0, color: Colors.black),
+                        ),
+                      ),
+                      child: Image(
+                        // 5초 후 이미지를 내려주는 샘플
+                        image: const NetworkImage(
+                            "http://127.0.0.1:8080/service1/tk/v1/file-test/client-image-test?delayTimeSecond=5"),
+                        fit: BoxFit.cover,
+                        loadingBuilder: (BuildContext context, Widget child,
+                            ImageChunkEvent? loadingProgress) {
+                          // 로딩 중일 때 플레이스 홀더를 보여줍니다.
+                          if (loadingProgress == null) {
+                            return child; // 로딩이 끝났을 경우
+                          }
+                          return Container(
+                            color: Colors.grey,
+                            child: const Center(
+                              child: CircularProgressIndicator(),
                             ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  const Center(
-                    child: Text(
-                      "5초 후 로딩",
-                      style: TextStyle(fontFamily: "MaruBuri"),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                width: 50,
-              ),
-              Column(
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    decoration: const BoxDecoration(
-                      border: Border(
-                        top: BorderSide(width: 1.0, color: Colors.black),
-                        bottom: BorderSide(width: 1.0, color: Colors.black),
-                        left: BorderSide(width: 1.0, color: Colors.black),
-                        right: BorderSide(width: 1.0, color: Colors.black),
+                          );
+                        },
+                        errorBuilder: (context, error, stackTrace) {
+                          // 에러 발생 시 설정한 에러 위젯을 반환합니다.
+                          return Container(
+                            color: Colors.grey,
+                            child: const Center(
+                              child: Icon(
+                                Icons.error,
+                                color: Colors.red,
+                              ),
+                            ),
+                          );
+                        },
                       ),
                     ),
-                    child: FadeInImage(
-                      fit: BoxFit.cover,
-                      placeholder: const AssetImage(
-                          "lib/assets/images/loading_spinner.gif"),
-                      image: const NetworkImage(
-                          "http://127.0.0.1:8080/service1/tk/v1/file-test/client-image-test?delayTimeSecond=-1"),
-                      imageErrorBuilder: (context, error, stackTrace) {
-                        return Container(
-                          color: Colors.grey,
-                          child: const Center(
-                            child: Icon(
-                              Icons.error,
-                              color: Colors.red,
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    const Center(
+                      child: Text(
+                        "5초 후 로딩",
+                        style: TextStyle(fontFamily: "MaruBuri"),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Column(
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          top: BorderSide(width: 1.0, color: Colors.black),
+                          bottom: BorderSide(width: 1.0, color: Colors.black),
+                          left: BorderSide(width: 1.0, color: Colors.black),
+                          right: BorderSide(width: 1.0, color: Colors.black),
+                        ),
+                      ),
+                      child: Image(
+                        // 이미지를 내려주지 않고 에러가 나는 샘플
+                        image: const NetworkImage(
+                            "http://127.0.0.1:8080/service1/tk/v1/file-test/client-image-test?delayTimeSecond=-1"),
+                        fit: BoxFit.cover,
+                        loadingBuilder: (BuildContext context, Widget child,
+                            ImageChunkEvent? loadingProgress) {
+                          // 로딩 중일 때 플레이스 홀더를 보여줍니다.
+                          if (loadingProgress == null) {
+                            return child; // 로딩이 끝났을 경우
+                          }
+                          return Container(
+                            color: Colors.grey,
+                            child: const Center(
+                              child: CircularProgressIndicator(),
                             ),
-                          ),
-                        );
-                      },
+                          );
+                        },
+                        errorBuilder: (context, error, stackTrace) {
+                          // 에러 발생 시 설정한 에러 위젯을 반환합니다.
+                          return Container(
+                            color: Colors.grey,
+                            child: const Center(
+                              child: Icon(
+                                Icons.error,
+                                color: Colors.red,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  const Center(
-                    child: Text(
-                      "에러 발생",
-                      style: TextStyle(fontFamily: "MaruBuri"),
+                    const SizedBox(
+                      height: 5,
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    const Center(
+                      child: Text(
+                        "에러 발생",
+                        style: TextStyle(fontFamily: "MaruBuri"),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
